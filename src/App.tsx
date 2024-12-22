355e090df04574f69a1791abb085b3ffc05ee10e
rnelson2 <rnelson2@users.noreply.github.com>
2024-12-22 11:38:03 -0500

    switched from browserrouter to hashrouter for development

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { Suspense, useState } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 // import DimensionsContextProvider from "./DimensionsContextProvider";
 // import AppStateContextProvider from "./AppStateContextProvider";
 import Masthead from "./Components/Masthead/Index";
