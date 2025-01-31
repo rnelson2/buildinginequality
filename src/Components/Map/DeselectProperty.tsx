f3a49013335c58dc2d3b5575056d9d19b82ca025
rnelson2 <rnelson2@users.noreply.github.com>
2025-01-31 09:43:16 -0500

    still troubleshooting github hashrouter issue

--- src/Components/Map/DeselectProperty.tsx
+++ src/Components/Map/DeselectProperty.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import { useMapEvent } from "react-leaflet";
 import * as L from 'leaflet';
-import { useSelectedPropertyData, useMapContext } from "../../hooks";
+import { useSelectedPropertyData, useMapContext, useURLState } from "../../hooks";
 
 const DeselectProperty = (): null => {
   const navigate = useNavigate();
   const selectedProperty = useSelectedPropertyData();
+  const { hash } = useURLState();
   const { map } = useMapContext();
 
   // if the map moves and the selected property is no longer visible, deselect it
@@ -15,7 +16,7 @@ const DeselectProperty = (): null => {
       const mapBounds = map.getBounds();
       const selectedPropertyLatLng = new L.LatLng(selectedProperty.geometry.coordinates[1], selectedProperty.geometry.coordinates[0]);
       if (!mapBounds.contains(selectedPropertyLatLng)) {
-        navigate(`${process.env.PUBLIC_URL}/map`);
+        navigate(`/map#${hash}`);
       }
     }
   });
