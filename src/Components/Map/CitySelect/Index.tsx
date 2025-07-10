1adabaf8a6843c17e49fdc0e174b74d59c3fa2a2
rnelson2 <rnelson2@users.noreply.github.com>
2025-07-09 21:34:17 -0400

    trying different strategy for city/map update that hopefully works more consistently

--- src/Components/Map/CitySelect/Index.tsx
+++ src/Components/Map/CitySelect/Index.tsx
@@ -2,8 +2,10 @@ import React from "react";
 import Select from "react-select";
 import { useCitiesOptions, useMapContext } from "../../../hooks";
 import * as Styled from "./styled";
-import { useDimensions } from "../../../hooks";
-import { polygonToBounds } from "../../../utilities";
+import { useDimensions, useURLState } from "../../../hooks";
+import { polygonToBounds, modifyHash } from "../../../utilities";
+import { useNavigate } from "react-router-dom";
+import { latLngBounds } from "leaflet";
 
 interface GroupedOption {
   label: string; // State name (non-selectable header)
@@ -21,8 +23,12 @@ interface Option {
 
 const CitySelect = () => {
   const { groupedOptions } = useCitiesOptions();
+  const navigate = useNavigate();
   const { device } = useDimensions();
   const { map } = useMapContext();
+  const { selectedProperty, hash } = useURLState();
+
+  console.log(hash);
 
   const handleChange = (selectedOption: any) => {
     if (selectedOption && map) {
@@ -36,9 +42,21 @@ const CitySelect = () => {
         bounds[0][1] -= 0.01;
         bounds[1][1] += 0.01;
       }
+      if (selectedProperty) {
+        navigate(`/map`);
+      } 
+      const leafletBounds = latLngBounds(bounds);
+      const newZoom = map.getBoundsZoom(leafletBounds);
+      const newCenter = leafletBounds.getCenter();
+      const newLat = Math.round(newCenter.lat * 10000) / 10000; // round to 4 decimal places
+      const newLng = Math.round(newCenter.lng * 10000) / 10000;
+      const newHash = modifyHash(hash, [
+        { type: 'set_loc', payload: { zoom: newZoom, center: [newLat, newLng] } },
+      ]);
+      navigate(`/map#${newHash}`, { replace: true });
+      
       // on desktop, pad the right boundary because of the legend
-      map.fitBounds(bounds, { maxZoom: 16, paddingBottomRight: (device === 'desktop') ? [213, 0] : [0, 0] }); // Fit the map to the selected city's bounds
-      //navigate(selectedOption.value); // Navigate to the selected city's link
+      //map.fitBounds(bounds, { maxZoom: 16, paddingBottomRight: (device === 'desktop') ? [213, 0] : [0, 0] }); // Fit the map to the selected city's bounds
     }
   };
 
